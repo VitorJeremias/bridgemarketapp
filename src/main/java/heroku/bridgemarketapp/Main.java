e4eb0129413dc211b55e41b4bf4ee11365022f3e
Osvaldo da Rocha <osvaldodarocha1992@yahoo.com.br>
2019-01-25 16:21:29 -0200

    refactoring, inicio querydsl sql

---
+++ src/main/java/heroku/bridgemarketapp/Main.java
@@ -0,0 +1,188 @@
+package heroku.bridgemarketapp;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.api.webhook.Payload;
+import com.mysema.query.sql.codegen.MetaDataExporter;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Controller
+@SpringBootApplication
+public class Main {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Autowired
+	private DataSource dataSource;
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@RequestMapping("/")
+	String index() {
+		return "index";
+	}
+
+	@RequestMapping("/generate_dsl")
+	public String generate() {
+
+		try (Connection connection = this.dataSource.getConnection()) {
+			MetaDataExporter exporter = new MetaDataExporter();
+			exporter.setPackageName("com.myproject.mydomain");
+			exporter.setTargetFolder(new File("src/main/java/heroku/bridgemarketapp/model"));
+			exporter.export(connection.getMetaData());
+
+			return "Success!";
+		} catch (Exception e) {
+			return "error";
+		}
+	}
+
+	@RequestMapping("/list")
+	@ResponseBody
+	public String list(Map<String, Object> model, @RequestParam(value = "text", required = false) String product) {
+		try (Connection connection = this.dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+
+			return this.listToBuy(stmt);
+		} catch (Exception e) {
+			model.put("message", e.getMessage());
+			return "ERROR - " + e.getMessage();
+		}
+	}
+
+	@RequestMapping("/db")
+	String db(Map<String, Object> model) {
+		try (Connection connection = this.dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp);");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now());");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks;");
+
+			ArrayList<String> output = new ArrayList<>();
+			while (rs.next()) {
+				output.add("Read from DB: " + rs.getTimestamp("tick"));
+			}
+
+			model.put("records", output);
+			return "db";
+		} catch (Exception e) {
+			model.put("message", e.getMessage());
+			return "error";
+		}
+	}
+
+	@RequestMapping("/buy")
+	@ResponseBody
+	public String buy(Map<String, Object> model, @RequestParam(value = "text", required = false) String product,
+			@RequestParam(value = "user_name", required = false) String userName) {
+		try (Connection connection = this.dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS tb_buy (ds_buy varchar, ds_user varchar);");
+			stmt.executeUpdate("INSERT INTO tb_buy VALUES ('" + product + "' ,'" + userName.substring(0, 1).toUpperCase() + userName.substring(1) + "');");
+
+			this.sendMessage(this.listToBuy(stmt));
+
+			return "Item adicionado com sucesso!";
+		} catch (Exception e) {
+			model.put("message", e.getMessage());
+			return "ERROR - " + e.getMessage();
+		}
+	}
+
+	@RequestMapping("/remove")
+	@ResponseBody
+	public String remove(Map<String, Object> model, @RequestParam(value = "text", required = false) String product,
+			@RequestParam(value = "user_name", required = false) String userName) {
+		try (Connection connection = this.dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("DELETE FROM tb_buy WHERE tb_buy.ds_buy = '" + product + "';");
+
+			this.sendMessage("-- " + userName.substring(0, 1).toUpperCase() + userName.substring(1) + " está indo comprar o item: " + product);
+
+			return "Item removido com sucesso!";
+		} catch (Exception e) {
+			model.put("message", e.getMessage());
+			return "ERROR - " + e.getMessage();
+		}
+	}
+
+	@RequestMapping("/remove_all")
+	@ResponseBody
+	public void removeAll(Map<String, Object> model, @RequestParam(value = "text", required = false) String product) {
+		try (Connection connection = this.dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("DROP TABLE tb_buy;");
+		} catch (Exception e) {
+			// do nothing
+		}
+	}
+
+	@RequestMapping("/going")
+	@ResponseBody
+	public void going(Map<String, Object> model, @RequestParam(value = "user_name", required = false) String userName) {
+		try {
+			this.sendMessage(userName.substring(0, 1).toUpperCase() + userName.substring(1) + " sinaliza que estará indo no mercado em breve. Façam seus pedidos!");
+		} catch (Exception e) {
+			// do nothing
+		}
+	}
+
+	private String listToBuy(Statement stmt) throws Exception {
+		ResultSet rs = stmt.executeQuery("SELECT ds_buy, ds_user FROM tb_buy;");
+
+		String output = "-- ITENS A SEREM COMPRADOS\n";
+		while (rs.next()) {
+			output += rs.getString("ds_buy") + " - " + rs.getString("ds_user") + "\n";
+		}
+
+		return output;
+	}
+
+	private void sendMessage(String value) throws Exception {
+		String url = "https://hooks.slack.com/services/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";
+
+		Payload payload = Payload.builder()
+				.channel("#mercado")
+				.username("Tô indo no mercado")
+				.text(value)
+				.build();
+
+		Slack slack = Slack.getInstance();
+		slack.send(url, payload);
+	}
+
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		if (this.dbUrl == null || this.dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(this.dbUrl);
+			return new HikariDataSource(config);
+		}
+	}
+
+}
